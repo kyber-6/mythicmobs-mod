69d882837f422a188b63dfb8b63b3393af211c10
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-09-23 19:32:03 +0200

    changes

--- src/main/java/net/pixeldream/mythicmobs/config/MythicMobsConfigs.java
+++ src/main/java/net/pixeldream/mythicmobs/config/MythicMobsConfigs.java
@@ -54,7 +54,7 @@ public class MythicMobsConfigs extends MidnightConfig {
     @MidnightConfig.Comment(centered = true)
     public static Comment miscellaneous;
     @MidnightConfig.Entry
-    public static boolean replaceIronGolems = true;
+    public static boolean replaceIronGolems = false;
     @MidnightConfig.Entry
     public static List<String> redMushroomLines = Arrays.asList(
             "playerGreeting",
