6546a866d1291055eca6927624173d45bc26b1cb
kyber-6 <100732009+kyber-6@users.noreply.github.com>
2023-11-03 18:49:23 +0100

    Add tags

--- src/main/java/net/pixeldream/mythicmobs/block/BronzeBlock.java
+++ src/main/java/net/pixeldream/mythicmobs/block/BronzeBlock.java
@@ -4,13 +4,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.pixeldream.mythicmobs.registry.BlockRegistry;
+import net.pixeldream.mythicmobs.registry.TagRegistry;
 
 public class BronzeBlock extends Block {
     public BronzeBlock(Settings settings) {
@@ -20,7 +20,7 @@ public class BronzeBlock extends Block {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         Item item = player.getStackInHand(Hand.MAIN_HAND).getItem();
-        if (item == Items.IRON_PICKAXE || item == Items.GOLDEN_PICKAXE || item == Items.DIAMOND_PICKAXE || item == Items.NETHERITE_PICKAXE) {
+        if (item.getRegistryEntry().isIn(TagRegistry.Items.PICKAXES)) {
             item.use(world, player, hand);
             world.addBlockBreakParticles(pos, this.getDefaultState());
             world.setBlockState(pos, BlockRegistry.CUT_BRONZE_BLOCK.getDefaultState());
